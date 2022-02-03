b392efaf99b3237c75c9094900f4c9c5802891f9
Raja Aadhithan <raja.aadhithan.t@gmail.com>
2022-02-03 16:31:02 +0530

    updated 2D

--- M.Tech_SV/Experiment 1/2D_Array.sv
+++ M.Tech_SV/Experiment 1/2D_Array.sv
@@ -1,11 +1,4 @@
 module array_2dim();
-  bit [11:0] array_2d[4][3]; //12 bit array of 4 rows and 3 columns
-  
-  initial begin
-    array_2d = '{'{0,1,2},'{3,4,5},'{6,7,8},'{19,20,21}}; //seperate literal for each row
-    
-    foreach(array_2d[i]) 
-      $display("Row %0d of array is ",i,array_2d[i][0],array_2d[i][1],array_2d[i][2]);
-  end
-endmodule
-  
+  bit [11:0] array_2d[4][3] = '{'{0,1,2},'{3,4,5},'{6,7,8},'{19,20,21}}; 
+  initial foreach(array_2d[i])  $display("Row %0d of array is ",i,array_2d[i][0],array_2d[i][1],array_2d[i][2]);
+endmodule 
