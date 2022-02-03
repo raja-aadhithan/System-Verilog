d6a6012d6f8aae1d374419b284419034ede93fc7
Raja Aadhithan <raja.aadhithan.t@gmail.com>
2022-02-03 16:21:08 +0530

    comment

--- M.Tech_SV/Experiment 1/12_bit_array.sv
+++ M.Tech_SV/Experiment 1/12_bit_array.sv
@@ -2,11 +2,11 @@ module array_methods();
   bit [11:0] my_array[4];
   
   initial begin
-  	my_array = '{12'h12,12'h345,12'h678,12'h9AB};
+  	my_array = '{12'h12,12'h345,12'h678,12'h9AB}; // array initialization
     
     $display("\nUsing for loop");
     for(int i=0; i<$size(my_array); i++) 
-      $displayh("my_array(%0d)=",i,my_array[i]);
+      $displayh("my_array(%0d)=",i,my_array[i]); // $dislayh displays hex values
     
     $display("\nUsing foreach loop");
     foreach(my_array[i]) 
@@ -15,7 +15,7 @@ module array_methods();
     $display("\n\nvalues of bits [5:4]");
     $display("\nUsing for loop");
     for(int i=0; i<$size(my_array); i++) 
-      $display("my_array(%0d)=%b",i,my_array[i][5:4]);
+      $display("my_array(%0d)=%b",i,my_array[i][5:4]); // prints the subset
     
     $display("\nUsing foreach loop");
     foreach(my_array[i]) 
@@ -24,7 +24,7 @@ module array_methods();
     $display("\n\nAdding 4 to each value");
     $display("\nUsing for loop");
     for(int i=0; i<$size(my_array); i++) begin
-      my_array[i] += 4;
+      my_array[i] += 4; //add 4 to each value
       $displayh("my_array(%0d)=",i,my_array[i]);
     end
     
--- M.Tech_SV/Experiment 1/2D_Array.sv
+++ M.Tech_SV/Experiment 1/2D_Array.sv
@@ -1,10 +1,10 @@
 module array_2dim();
-  bit [11:0] array_2d[4][3];
+  bit [11:0] array_2d[4][3]; //12 bit array of 4 rows and 3 columns
   
   initial begin
-    array_2d = '{'{0,1,2},'{3,4,5},'{6,7,8},'{19,20,21}};
+    array_2d = '{'{0,1,2},'{3,4,5},'{6,7,8},'{19,20,21}}; //seperate literal for each row
     
-    foreach(array_2d[i])
+    foreach(array_2d[i]) 
       $display("Row %0d of array is ",i,array_2d[i][0],array_2d[i][1],array_2d[i][2]);
   end
 endmodule
